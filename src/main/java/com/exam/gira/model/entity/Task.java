90e47df1dedd31829e1c0ddc023a3055852726b9
sylvianikol <silviaadvert@gmail.com>
2021-02-18 08:04:31 +0200

    added Tasks

---
+++ src/main/java/com/exam/gira/model/entity/Task.java
@@ -0,0 +1,78 @@
+package com.exam.gira.model.entity;
+
+import com.exam.gira.model.enums.Progress;
+
+import javax.persistence.*;
+import javax.validation.constraints.Future;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+public class Task extends BaseEntity {
+
+    private String name;
+    private String description;
+    private Progress progress;
+    private LocalDate dueDate;
+    private Classification classification;
+    private User user;
+
+    public Task() {
+    }
+
+    @Column(nullable = false, unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(nullable = false)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Enumerated(value = EnumType.STRING)
+    public Progress getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Progress progress) {
+        this.progress = progress;
+    }
+
+    @Column(name = "due_date")
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Column(nullable = false)
+    @ManyToOne(optional=false)
+    public Classification getClassification() {
+        return classification;
+    }
+
+    public void setClassification(Classification classification) {
+        this.classification = classification;
+    }
+
+    @Column(nullable = false)
+    @ManyToOne(optional=false)
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}

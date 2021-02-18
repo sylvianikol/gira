9ba348ae175d6ed2dd3531f86b2c694f64dcae10
sylvianikol <silviaadvert@gmail.com>
2021-02-18 08:10:14 +0200

    added UserService

---
+++ src/main/java/com/exam/gira/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.exam.gira.service.impl;
+
+import com.exam.gira.repository.UserRepository;
+import com.exam.gira.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+}

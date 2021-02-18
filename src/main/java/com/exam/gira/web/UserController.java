eef6e6b3732c26382312671f8547225efdef4bbd
sylvianikol <silviaadvert@gmail.com>
2021-02-18 08:42:37 +0200

    created UserRegisterBindingModel, UserController, added front-end validation

---
+++ src/main/java/com/exam/gira/web/UserController.java
@@ -0,0 +1,33 @@
+package com.exam.gira.web;
+
+import com.exam.gira.model.binding.UserRegisterBindingModel;
+import com.exam.gira.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        if (!model.containsAttribute("userRegisterBindingModel")) {
+            model.addAttribute("userRegisterBindingModel", new UserRegisterBindingModel());
+            model.addAttribute("exists", false);
+        }
+        return "register";
+    }
+}

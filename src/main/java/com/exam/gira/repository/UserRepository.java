1aaf7e75e3ecb502e1b17c115745c99235c478b0
sylvianikol <silviaadvert@gmail.com>
2021-02-18 08:05:29 +0200

    added UserRepository

---
+++ src/main/java/com/exam/gira/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.exam.gira.repository;
+
+import com.exam.gira.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

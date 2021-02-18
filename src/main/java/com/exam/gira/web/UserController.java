82a6358ac8d1cb536a7a56deed33b7c6323c5e8b
sylvianikol <silviaadvert@gmail.com>
2021-02-18 08:44:07 +0200

    added post mapping for register

--- src/main/java/com/exam/gira/web/UserController.java
+++ src/main/java/com/exam/gira/web/UserController.java
@@ -6,8 +6,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -29,5 +34,31 @@ public class UserController {
             model.addAttribute("exists", false);
         }
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerConfirm(@Valid UserRegisterBindingModel userRegisterBindingModel,
+                                  BindingResult bindingResult,
+                                  RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors() || !userRegisterBindingModel.getPassword().equals(userRegisterBindingModel.getConfirmPassword())) {
+            redirectAttributes.addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel);
+            redirectAttributes.addFlashAttribute(
+                    "org.springframework.validation.BindingResult.userRegisterBindingModel",
+                    bindingResult);
+            return "redirect:register";
+        }
+
+        boolean isRegistered = this.userService.register(this.modelMapper.map(
+                userRegisterBindingModel, UserServiceModel.class));
+
+        if (!isRegistered) {
+            redirectAttributes.addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel);
+            redirectAttributes.addFlashAttribute("exists", true);
+
+            return "redirect:register";
+        }
+
+        return "redirect:login";
     }
 }
